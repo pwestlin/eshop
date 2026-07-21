7e52206605fb6dfc88da9ef1f1d2c75e0aba9638
pevest <pevest@lm.se>
2026-07-21 10:38:59 +0200

    Städ

--- src/main/kotlin/nu/westlin/eshop/Application.kt
+++ src/main/kotlin/nu/westlin/eshop/Application.kt
@@ -3,8 +3,6 @@ package nu.westlin.eshop
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-// TODO pwestlin: Jag var tvungen att slå av dessa för att få vissa tester att funka. Minns inte ens varför jag lade till dem...
-// @Modulithic(sharedModules = [/*Modules.CONFIG*//*, Modules.SECURITY*/])
 @SpringBootApplication
 class Application
 
